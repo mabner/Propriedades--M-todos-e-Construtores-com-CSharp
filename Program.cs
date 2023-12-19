74f7a2adc1fda817328b36bbf48a084e01dd81aa
Marcos Leite <m.abner@live.co.uk>
2023-12-19 01:48:12 -0300

    validating system region config for date format compliance

--- Program.cs
+++ Program.cs
@@ -1,33 +1,52 @@
 ﻿using System.Globalization;
 using ExploringCSharp.Models;
 
-Person p1 = new Person("Marcos", "Leite");
+//Person p1 = new Person("Marcos", "Leite");
 
 // Using parameter names in order to make the code clearer to read
 Person p2 = new Person(name: "Leyd", surname: "Leite");
 
-Course englishCourse = new Course();
-englishCourse.Name = "British English for Foreigners";
-englishCourse.Students = new List<Person>();
+// Course englishCourse = new Course();
+// englishCourse.Name = "British English for Foreigners";
+// englishCourse.Students = new List<Person>();
 
-englishCourse.AddStudent(p1);
-englishCourse.AddStudent(p2);
-englishCourse.ListStudents();
+// englishCourse.AddStudent(p1);
+// englishCourse.AddStudent(p2);
+// englishCourse.ListStudents();
 
 // Currency format
 CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
 
-decimal exchangeRate = 6.40M;
-Console.WriteLine($"{exchangeRate:C}");
-Console.WriteLine(exchangeRate.ToString("C", CultureInfo.CreateSpecificCulture("en-GB")));
+// decimal exchangeRate = 6.40M;
+// Console.WriteLine($"{exchangeRate:C}");
+// Console.WriteLine(exchangeRate.ToString("C", CultureInfo.CreateSpecificCulture("en-GB")));
 
-double successRate = .9682;
-System.Console.WriteLine(successRate.ToString("P"));
+// double successRate = .9682;
+// System.Console.WriteLine(successRate.ToString("P"));
 
-int agencyCode = 153694;
-System.Console.WriteLine(agencyCode.ToString("##-##-##"));
+// int agencyCode = 153694;
+// System.Console.WriteLine(agencyCode.ToString("##-##-##"));
 
-DateTime enrolmentDate = DateTime.Now;
+//DateTime enrolmentDate = DateTime.Now;
 
-System.Console.WriteLine(enrolmentDate.ToString(@"dddd, dd \de MMMM \de yyyy"));
+//System.Console.WriteLine(enrolmentDate.ToString(@"dddd, dd \de MMMM \de yyyy"));
 
+// Date with TryParse
+string dateString = DateTime.Now.ToShortDateString();
+
+bool correctDate = DateTime.TryParseExact(
+    dateString,
+    "yyyy-MM-dd",
+    CultureInfo.InvariantCulture,
+    DateTimeStyles.None,
+    out DateTime date
+);
+
+if (correctDate)
+{
+    System.Console.WriteLine($"System date: {date.ToShortDateString()}.");
+}
+else
+{
+    System.Console.WriteLine("Please review your region datetime format.");
+}
